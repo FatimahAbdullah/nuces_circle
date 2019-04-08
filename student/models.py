3202be87ca8bc2f05fc18a5ec4eccca64d4315e9
Fatimah <fatimah.a237@gmail.com>
2019-04-08 16:59:42 +0500

    added models and home shows up

--- student/models.py
+++ student/models.py
@@ -1,7 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from company.models import Company
 # Create your models here.
-class student(models.Model):
+class Student(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
+	bio = models.TextField(max_length=500, blank=True)
+	connections = models.ManyToManyField("self")
+
+class Post(models.Model):
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+	body = models.TextField()
+	date = models.DateTimeField(auto_now_add=True)
+
+class Accomplishment(models.Model):
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+	type = models.CharField(max_length=100)
+	description=models.TextField()
+
+class Skill(models.Model):
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+	name=models.CharField(max_length=150)
+
+class Interest(models.Model):
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+	company = models.OneToOneField(Company, on_delete=models.CASCADE)
+
+class Endorsement(models.Model):
+	skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+	person = models.OneToOneField(Student, on_delete=models.CASCADE)
+
+
+class Job(models.Model):
+    company=models.ForeignKey(Company, on_delete=models.CASCADE)
+    description=models.TextField()
+    applicants=models.ManyToManyField(Student, through='application',through_fields=('job','student'))
+
+class Application(models.Model):
+    job=models.ForeignKey(Job, on_delete=models.CASCADE)
+    student=models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
